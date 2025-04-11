7e1523ec1070343b7af213236502a939b780c303
flash3105 <80648288+flash3105@users.noreply.github.com>
2025-04-11 14:50:07 +0200

    Update Sidebar.js

--- src/Components/Sidebar.js
+++ src/Components/Sidebar.js
@@ -51,11 +51,11 @@ const Sidebar = ({ studentNumber }) => {
             <span>Home</span>
           </Link>
         </li>
-        <li className={getSelectedKey() === '2' ? 'selected' : ''}>
+       /* <li className={getSelectedKey() === '2' ? 'selected' : ''}>
           <Link to="/bookings">
             <CalendarOutlined className="menu-icon" />
             <span>Bookings</span>
-          </Link>
+          </Link>*/
         </li>
         <li className={getSelectedKey() === '3' ? 'selected' : ''}>
           <Link to="/analytics">
